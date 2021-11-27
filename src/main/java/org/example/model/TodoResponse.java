75ed58cef9126b6860a477e9c102cba166605ea1
DESKTOP-8H087C4\jung <qaqa313a@gmail.com>
2021-11-27 20:35:17 +0900

    Feat: implemention Model & dto

---
+++ src/main/java/org/example/model/TodoResponse.java
@@ -0,0 +1,27 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoResponse {
+
+    private Long id;
+    private String title;
+    private Long order;
+    private Boolean completed;
+    private String url;
+
+    public TodoResponse(TodoEntity todoEntity) {
+        this.id = todoEntity.getId();
+        this.title = todoEntity.getTitle();
+        this.order = todoEntity.getOrder();
+        this.completed = todoEntity.getCompleted();
+
+        // url은 기본적으로 property나 환경변수로 잡아주자.
+        this.url = "http://localhost:8080/" + this.id;
+    }
+}
